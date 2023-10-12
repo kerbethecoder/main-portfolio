f893b5ea5d5242ff06bca3b48012036d32d15fc8
kerbethecoder <krbycnts@gmail.com>
2023-10-12 12:00:09 +0800

    minor fix on seo

--- src/components/Seo.jsx
+++ src/components/Seo.jsx
@@ -1,8 +1,8 @@
 import React from "react"
 import { Helmet } from "react-helmet"
+import { graphql, useStaticQuery } from "gatsby"
 
 import favicon from "../images/favicon.png"
-import { graphql, useStaticQuery } from "gatsby"
 
 const SEO = () => {
   const { site } = useStaticQuery(graphql`
@@ -23,24 +23,22 @@ const SEO = () => {
   const url = site.siteMetadata.siteUrl
 
   return (
-    <>
-      <Helmet>
-        <html lang="en" />
-        <title>{siteTitle}</title>
-        <link rel="icon" href={favicon} />
-        <link rel="canonical" href={url} />
-        <meta charSet="UTF-8" />
-        <meta name="description" content={metaDescription} />
-        <meta name="url" content={url} />
-        <meta property="og:title" content={siteTitle} />
-        <meta property="og:description" content={metaDescription} />
-        <meta property="og:url" content={url} />
-        <meta property="og:type" content="website" />
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:title" content={siteTitle} />
-        <meta property="twitter:description" content={metaDescription} />
-      </Helmet>
-    </>
+    <Helmet>
+      <html lang="en" />
+      <title>{siteTitle}</title>
+      <link rel="icon" href={favicon} />
+      <link rel="canonical" href={url} />
+      <meta charSet="UTF-8" />
+      <meta name="description" content={metaDescription} />
+      <meta name="url" content={url} />
+      <meta property="og:title" content={siteTitle} />
+      <meta property="og:description" content={metaDescription} />
+      <meta property="og:url" content={url} />
+      <meta property="og:type" content="website" />
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:title" content={siteTitle} />
+      <meta property="twitter:description" content={metaDescription} />
+    </Helmet>
   )
 }
 
