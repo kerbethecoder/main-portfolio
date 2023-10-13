693f1f0449bd8fabd365383218efdc454bbb6a93
kerbethecoder <krbycnts@gmail.com>
2023-10-13 12:19:40 +0800

    minor fix

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -21,7 +21,7 @@ const index = () => {
       <Timeline />
       <Projects />
       <div className="flexCenter mb-20 lg:mb-40">
-        <a href="/resume.pdf" target="__blank" rel="noopener noreferrer">
+        <a href="/resume.pdf" target="_blank" rel="noopener noreferrer">
           <button className="btn__resume">View my Résumé 👌</button>
         </a>
       </div>
