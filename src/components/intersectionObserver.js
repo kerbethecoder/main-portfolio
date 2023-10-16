d62a0e2dd284c761430f92c3f555cde3c0c10f03
kerbethecoder <krbycnts@gmail.com>
2023-10-16 18:18:41 +0800

    minor fix; uninstalled dependency

--- src/components/intersectionObserver.js
+++ src/components/intersectionObserver.js
@@ -1,33 +1,3 @@
-// let observer
-
-// if ("IntersectionObserver" in window) {
-//   observer = new IntersectionObserver(entries => {
-//     entries.forEach(entry => {
-//       if (entry.isIntersecting) {
-//         entry.target.classList.add("showElem")
-//       } else {
-//         entry.target.classList.remove("showElem")
-//       }
-//     })
-//   })
-// } else {
-//   import("intersection-observer").then(() => {
-//     observer = new IntersectionObserver(entries => {
-//       entries.forEach(entry => {
-//         if (entry.isIntersecting) {
-//           entry.target.classList.add("showElem")
-//         } else {
-//           entry.target.classList.remove("showElem")
-//         }
-//       })
-//     })
-//   })
-// }
-
-// export function observeHiddenElements(element) {
-//   element.forEach(el => observer.observe(el))
-// }
-
 let observer
 
 if (typeof window !== "undefined" && "IntersectionObserver" in window) {
